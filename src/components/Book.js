bb8fad5e5d825bf2e37786576575b657da1471da
Rayan84 <rayan_84@live.com>
2021-11-10 14:13:47 +0100

    Add Book Component

---
+++ src/components/Book.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Book = (title, author) => (
+  <div>
+    <p>{title}</p>
+    <p>{author}</p>
+  </div>
+);
+export default Book;
--- src/components/Booklist.js
+++ src/components/Booklist.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import AddNewBook from './AddNewBook';
+import books from './Book';
 
-const Booklist = () => (
+const Booklist = (books) => (
   <div>
+    books.map(book) => (
+      <li key={book.id}>
+        <p>{book.title}</p>
+        <p>{book.author}</p>
+        <button>Delete</button>
+      </li>
+    )
     <AddNewBook />
     <h1>Book List</h1>
   </div>
