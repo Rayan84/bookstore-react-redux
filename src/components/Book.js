abfd5f640abdf18429f1deecd29053e9952b3174
Rayan84 <rayan_84@live.com>
2021-11-12 22:15:03 +0100

    Add logic for creating and removing entries

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import { React, useState } from 'react';
+import { v4 as numGenerate } from 'uuid';
+import { useDispatch } from 'react-redux';
+// import { uuid } from 'uuidv4';
+import { addBook } from '../redux/books/books';
 
 const Book = () => {
+  const dispatch = useDispatch();
+  const [author, setAuthor] = useState();
+  const [title, setTitle] = useState();
+  const submitBookToStore = () => {
+    const newBook = {
+      id: numGenerate(),
+      title,
+      author,
+    };
+    console.log(newBook.id);
+    dispatch(addBook(newBook));
+  };
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleAuthorChange = (event) => {
+    setAuthor(event.target.value);
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
+  //  submitBookToStore();
   };
 
   return (
@@ -10,12 +36,12 @@ const Book = () => {
       <h1>Add New Book</h1>
       <form action="" onSubmit={handleSubmit}>
         <label htmlFor="title">
-          <input type="text" id="title" />
+          <input type="text" id="title" onChange={handleAuthorChange} />
         </label>
         <label htmlFor="author">
-          <input type="text" id="author" />
+          <input type="text" id="author" onChange={handleTitleChange} />
         </label>
-        <input type="submit" />
+        <button type="submit" onClick={submitBookToStore}>Add Book</button>
       </form>
     </div>
   );
--- src/components/Booklist.js
+++ src/components/Booklist.js
@@ -1,24 +1,42 @@
-import React from 'react';
+import { React } from 'react';
+// import { React } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Book from './Book';
+import { removeBook } from '../redux/books/books';
 
 const Booklist = () => {
-  const books = [
-    {
-      title: 'The title',
-      author: 'The author',
-      id: 1,
-    },
-  ];
+  const dispatch = useDispatch();
+  const books = useSelector((state) => Object.values(state));
+  console.log(books);
+  console.log(books);
+  console.log(Object.values(books[0]));
+  const items = Object.values(books[0]);
+  console.log(items);
+
+  const handleDelete = (item) => {
+    console.log('+++++++');
+    console.log(item);
+    dispatch(removeBook(item));
+  };
+
   return (
     <div>
       <Book />
       <h1>Book List</h1>
       <div>
-        {books.map((book) => (
+        {/* {items.map((book) => (
           <ul key={book.id}>
             <li>{book.title}</li>
             <li>{book.author}</li>
+            <button type="button" onClick={() => { handleDelete(book.id); }}>Remove Book</button>
           </ul>
+        ))} */}
+        {items.map((book) => (
+          <div key={book.id}>
+            <span>{book.title}</span>
+            <span>{book.author}</span>
+            <button type="button" onClick={() => { handleDelete(book.id); }}>Remove Book</button>
+          </div>
         ))}
       </div>
     </div>
