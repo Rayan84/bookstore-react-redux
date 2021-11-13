13c14a0aa01343607ab233f08bcbe166790e2e80
Rayan84 <rayan_84@live.com>
2021-11-13 19:35:10 +0100

    Add logic to connect API

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,48 +1,29 @@
-import { React, useState } from 'react';
-import { v4 as numGenerate } from 'uuid';
+import React from 'react';
+import { Row, Col, Button } from 'react-bootstrap';
+import { PropTypes } from 'prop-types';
 import { useDispatch } from 'react-redux';
-// import { uuid } from 'uuidv4';
-import { addBook } from '../redux/books/books';
+import { removeBook } from '../redux/books/books';
 
-const Book = () => {
+export default function Book(props) {
   const dispatch = useDispatch();
-  const [author, setAuthor] = useState();
-  const [title, setTitle] = useState();
-  const submitBookToStore = () => {
-    const newBook = {
-      id: numGenerate(),
-      title,
-      author,
-    };
-    dispatch(addBook(newBook));
-  };
-
-  const handleTitleChange = (event) => {
-    setTitle(event.target.value);
-  };
-
-  const handleAuthorChange = (event) => {
-    setAuthor(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-  //  submitBookToStore();
-  };
-
+  const { data } = props;
   return (
     <div>
-      <h1>Add New Book</h1>
-      <form action="" onSubmit={handleSubmit}>
-        <label htmlFor="title">
-          <input type="text" id="title" onChange={handleAuthorChange} />
-        </label>
-        <label htmlFor="author">
-          <input type="text" id="author" onChange={handleTitleChange} />
-        </label>
-        <button type="submit" onClick={submitBookToStore}>Add Book</button>
-      </form>
+      <Row className="m-5 border border-2 bg-white">
+        <Col className="p-4 m-auto">
+          <h5>{data.title}</h5>
+          {' '}
+          <h6>{data.category}</h6>
+        </Col>
+        <Col className="p-4 m-auto"><Button onClick={() => { dispatch(removeBook(data)); }} className="bg-danger border border-0">Remove</Button></Col>
+      </Row>
     </div>
   );
+}
+
+Book.propTypes = {
+  data: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+  }).isRequired,
 };
-export default Book;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import store from './redux/configureStore';
+import App from './components/App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
